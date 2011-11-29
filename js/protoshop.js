a927a509af80d7c25acf9c14bb4834f3864972c3
Dale Harvey <dale@arandomurl.com>
2011-11-29 08:21:00 +0000

    Moved plain dom to objects

--- js/protoshop.js
+++ js/protoshop.js
@@ -2,58 +2,213 @@ var shortcuts = {
   global : {
     description: 'Global Shortcuts',
     shortcuts: [
-      {key: 'esc', e: 'keydown',
-       callback: function() { $('#panel').toggle(); },
-       description: 'Preview Design'},
-      {key: '?', e:'keypress', override: 'shift+?',
-       callback: function() { $('#keyboard-help').toggle(); },
-       description: 'Show / Hide the help dialog'}
+      {
+        key: 'esc',
+        e: 'keydown',
+        callback: function() { $('#panel').toggle(); },
+        description: 'Preview Design'
+      },
+      {
+        key: '?',
+        e:'keypress',
+        override: 'shift+?',
+        callback: function() { $('#keyboard-help').toggle(); },
+        description: 'Show / Hide the help dialog'}
+    ]
+  },
+  editing: {
+    description: 'Editing Item Shortcuts',
+    shortcuts: [
+      {
+        key: 'left',
+        e: 'keydown',
+        callback: function() {
+          this.selected.css({left: this.selected.$dom.position().left - 1});
+        },
+        description: 'Move Left'
+      },
+      {
+        key: 'right',
+        e: 'keydown',
+        callback: function() {
+          this.selected.css({left: this.selected.$dom.position().left + 1});
+        },
+        description: 'Move Right'
+      },
+      {
+        key: 'up',
+        e: 'keydown',
+        callback: function() {
+          this.selected.css({top: this.selected.$dom.position().top - 1});
+        },
+        description: 'Move Up'
+      },
+      {
+        key: 'down',
+        e: 'keydown',
+        callback: function() {
+          this.selected.css({top: this.selected.$dom.position().top + 1});
+        },
+        description: 'Move Down'
+      },
+      {
+        key: 'backspace',
+        e: 'keydown',
+        callback: function() {
+          this.selected.$dom.remove();
+          this.selectElement(null);
+        },
+        description: 'Delete Element'
+      },
+      {
+        key: 'shift+left',
+        e: 'keydown',
+        callback: function() {
+          this.selected.css({width: this.selected.$dom.width() - 1});
+        },
+        description: 'Decrease Width'
+      },
+      {
+        key: 'shift+right',
+        e: 'keydown',
+        callback: function() {
+          this.selected.css({width: this.selected.$dom.width() + 1});
+        },
+        description: 'Increase Width'
+      },
+      {
+        key: 'shift+up',
+        e: 'keydown',
+        callback: function() {
+          this.selected.css({height: this.selected.$dom.height() - 1});
+        },
+        description: 'Decrease Height'
+      },
+      {
+        key: 'shift+down',
+        e: 'keydown',
+        callback: function() {
+          this.selected.css({height: this.selected.$dom.height() + 1});
+        },
+        description: 'Increase Height'
+      }
     ]
   }
 };
 
-var rr = (function() {
 
+    // keyBind('keymove', {
+    //   'Shift+Left'  : function() { $el.css({width: $el.width() - 1}); },
+    //   'Shift+Right' : function() { $el.css({width: $el.width() + 1}); },
+    //   'Shift+Up'    : function() { $el.css({height: $el.height() - 1}); },
+    //   'Shift+Down'  : function() { $el.css({height: $el.height() + 1}); },
+    // });
+
+var CoreElement = function() {
+
+  var $handles = $('<div class="handles">' +
+    '<div class="top-left" data-handle="nw" data-type="handle"></div>' +
+    '<div class="top-right" data-handle="ne" data-type="handle"></div>' +
+    '<div class="bottom-left" data-handle="sw" data-type="handle"></div>' +
+    '<div class="bottom-right" data-handle="se" data-type="handle"></div>' +
+    '<div class="top" data-handle="n" data-type="handle"></div>' +
+    '<div class="right" data-handle="e" data-type="handle"></div>' +
+    '<div class="bottom" data-handle="s" data-type="handle"></div>' +
+    '<div class="left" data-handle="w" data-type="handle"></div>' +
+    '</div>');
+
+  var $info = $('<div class="info-box"></div>');
+
+  var $dom = this.$dom = null;
+
+  this.select = function() {
+    this.$dom.addClass('selected');
+    this.$dom.append($handles);
+    this.$dom.append($info);
+  };
+
+  this.deselect = function() {
+    this.$dom.removeClass('selected');
+    $handles.remove();
+    $info.remove();
+  };
+
+  this.updateInfo = function() {
+    $info.text("y:" + this.$dom.position().top + " x:" +
+               this.$dom.position().left +
+               " [" + this.$dom.width() + "x" + this.$dom.height() + "]");
+  };
+
+  this.css = function(obj) {
+    this.$dom.css(obj);
+    this.updateInfo();
+  };
+};
+
+var BlockElement = function(index) {
+  this.$dom = $('<div>', {
+    'z-index': index,
+    'data-type': 'block',
+    'class': 'block'
+  });
+  this.$dom.data('obj', this);
+};
+BlockElement.prototype = new CoreElement();
+
+var TextElement = function(index) {
+  this.$dom = $('<div>', {
+    'z-index': index,
+    'data-type': 'text',
+    'class': 'text'
+  }).append('<span>text</span>');
+  this.$dom.data('obj', this);
+};
+TextElement.prototype = new CoreElement();
+
+var Protoshop = function() {
+
+  var self = this;
   var $canvas = $('#canvas');
-  var $selected = null;
 
-  function selectElement($el) {
+  this.selected = null;
 
-    if ($selected) {
+  this.selectElement = function(el) {
+
+    if (self.selected) {
+      self.selected.deselect();
       $(document).unbind('.keymove');
-      $selected.removeClass('selected');
 
-      if ($selected.data('deselect')) {
-        $selected.data('deselect')();
-        $selected.data('deselect', null);
+      if (self.selected.$dom.data('deselect')) {
+        self.selected.$dom.data('deselect')();
+        self.selected.$dom.data('deselect', null);
       }
     }
 
-    $selected = $el;
+    self.selected = el;
 
-    if ($el) {
-      $selected.addClass('selected');
-      bindKeyMove($selected);
+    if (el) {
+      var $el = el.$dom;
+      el.select();
+      bindKeyMove(self.selected.$dom);
     }
-  }
+  };
 
-  function bindMouseMove($el, e) {
+  function bindMouseMove(e) {
 
     var offset = {
-      left: e.clientX - $selected.position().left,
-      top: e.clientY - $selected.position().top
+      left: e.clientX - self.selected.$dom.position().left,
+      top: e.clientY - self.selected.$dom.position().top
     };
 
-    var $info = $el.find('.info-box');
-
-    $canvas.bind('mousemove.moving', function(e) {
-      $el.css({left: e.clientX - offset.left, top: e.clientY - offset.top});
-      $info.text("y:" + $el.position().top + " x:" + $el.position().left +
-                 " [" + $el.width() + "x" + $el.height() + "]");
+    $canvas.bind('mousemove.editing', function(e) {
+      self.selected.css({
+        left: e.clientX - offset.left,
+        top: e.clientY - offset.top
+      });
     });
 
     $canvas.bind('mouseup.moving', function(e) {
-      $canvas.unbind('.moving');
+      $canvas.unbind('.editing');
     });
 
   }
@@ -61,7 +216,10 @@ var rr = (function() {
 
   function bindMouseResize($el, e, type) {
 
-    var size = {width: $selected.width(), height: $selected.height()};
+    var size = {
+      width: self.selected.$dom.width(),
+      height: self.selected.$dom.height()
+    };
 
     var offset = {
       left: e.pageX - size.width,
@@ -86,17 +244,12 @@ var rr = (function() {
       }
     };
 
-    var $info = $el.find('.info-box');
-
     $canvas.bind('mousemove.resize', function(e) {
       var obj = {}, i;
       for(i = 0; i < len; i++) {
         resize[type[i]](e, obj);
       }
-      $el.css(obj);
-      $info.text("y:" + $el.position().top + " x:" + $el.position().left +
-                 " [" + $el.width() + "x" + $el.height() + "]");
-
+      self.selected.css(obj);
     });
 
     $canvas.bind('mouseup.moving', function(e) {
@@ -105,17 +258,18 @@ var rr = (function() {
 
   }
 
+  function bind(scope, fn) {
+    return function (evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      if (fn.apply(scope, arguments) !== false) {
+      }
+    };
+  }
+
   function bindKeyMove($el) {
-    keyBind('keymove', {
-      'Shift+Left'  : function() { $el.css({width: $el.width() - 1}); },
-      'Shift+Right' : function() { $el.css({width: $el.width() + 1}); },
-      'Shift+Up'    : function() { $el.css({height: $el.height() - 1}); },
-      'Shift+Down'  : function() { $el.css({height: $el.height() + 1}); },
-      'Left'        : function() { $el.css({left: $el.position().left - 1}); },
-      'Right'       : function() { $el.css({left: $el.position().left + 1}); },
-      'Up'          : function() { $el.css({top: $el.position().top - 1}); },
-      'Down'        : function() { $el.css({top: $el.position().top + 1}); },
-      'Backspace'   : function() { $el.remove(); selectElement(null); }
+    _.each(shortcuts.editing.shortcuts, function(key) {
+      $(document).bind(key.e + '.editing', key.override || key.key, bind(self, key.callback));
     });
   }
 
@@ -123,9 +277,10 @@ var rr = (function() {
   $canvas.bind('dblclick', function(e) {
 
     var $targ = $(e.target);
+    var obj = $targ.data('obj');
 
-    if ($targ.data('type') === 'text') {
-      $selected = $targ;
+    if (obj instanceof TextElement) {
+      self.selected = obj;
       var text = $targ.find('span').text();
       var $inp = $('<textarea>' + text + '</textarea>');
       $targ.find('span').replaceWith($inp);
@@ -143,17 +298,20 @@ var rr = (function() {
   $canvas.bind('mousedown', function(e) {
 
     if (e.target === this) {
-      selectElement(null);
+      self.selectElement(null);
       return true;
     }
 
     var $el = $(e.target);
+    var obj = $el.data('obj');
 
-    if ($el.data('type') === 'block' || $el.data('type') === 'text') {
+    if (obj instanceof CoreElement) {
       e.preventDefault();
       e.stopPropagation();
-      selectElement($el);
-      bindMouseMove($el, e);
+      if (!$el.is('.selected')) {
+        self.selectElement($el.data('obj'));
+      }
+      bindMouseMove(e);
     }
 
     if ($el.data('type') === 'handle') {
@@ -164,22 +322,10 @@ var rr = (function() {
 
   });
 
-  _.each(shortcuts, function(section) {
-    _.each(section.shortcuts, function(key) {
-      $(document).bind(key.e, key.override || key.key, key.callback);
-    });
+  _.each(shortcuts.global.shortcuts, function(key) {
+    $(document).bind(key.e, key.override || key.key, key.callback);
   });
 
-  var handles = '<div class="handles">' +
-    '<div class="top-left" data-handle="nw" data-type="handle"></div>' +
-    '<div class="top-right" data-handle="ne" data-type="handle"></div>' +
-    '<div class="bottom-left" data-handle="sw" data-type="handle"></div>' +
-    '<div class="bottom-right" data-handle="se" data-type="handle"></div>' +
-    '<div class="top" data-handle="n" data-type="handle"></div>' +
-    '<div class="right" data-handle="e" data-type="handle"></div>' +
-    '<div class="bottom" data-handle="s" data-type="handle"></div>' +
-    '<div class="left" data-handle="w" data-type="handle"></div>' +
-    '</div><div class="info-box">5 x 5</div>';
 
   (function() {
 
@@ -187,20 +333,14 @@ var rr = (function() {
 
     var panelFuns = {
       'add-block': function() {
-        var $el = $('<div>', {
-          'z-index': ++index,
-          'data-type': 'block',
-          'class': 'block'
-        }).appendTo($canvas).append(handles);
-        selectElement($el);
+        var el = new BlockElement(++index);
+        el.$dom.appendTo($canvas);
+        self.selectElement(el);
       },
       'add-text': function() {
-        var $el = $('<div>', {
-          'z-index': ++index,
-          'data-type': 'text',
-          'class': 'text'
-        }).appendTo($canvas).append('<span>text</span>').append(handles);
-        selectElement($el);
+        var el = new TextElement(++index);
+        el.$dom.appendTo($canvas);
+        self.selectElement(el);
       }
     };
 
@@ -234,5 +374,7 @@ var rr = (function() {
   $('#keyboard-placer').html(html.join(''));
 
 
-})();
+};
+
+var x = new Protoshop();
 
